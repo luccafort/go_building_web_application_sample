80645fba7ac186a8b29b684cdb6a820e03493176
luccafort <luccafort@gmail.com>
2017-01-05 17:23:13 +0900

    [modify]チャットルームの並行化
    並行プログラミングを行うためにrunメソッドを追加
    チャットルームをHTTPハンドラ化
    ServeHTTPメソッドの追加

--- room.go
+++ room.go
@@ -1,8 +1,67 @@
 package main
 
+import (
+	"net/http"
+
+	"log"
+
+	"github.com/gorilla/websocket"
+)
+
 type room struct {
 	forward chan []byte      // forwardは他のクライアントに転送するためにメッセージを保持するチャネル
 	join    chan *client     // join はチャットルームに参加しようとしているクライアントのためのチャネル
 	leave   chan *client     // leave はチャットルームから退室しようとしているクライアントのためのチャネル
 	clients map[*client]bool // clients には在室している全てのクライアントが保持されます
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+		case client := <-r.leave:
+			// 退室
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// 全てのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+				// メッセージ送信
+				default:
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() {
+		r.leave <- client
+	}()
+	go client.write()
+	client.read()
 }
