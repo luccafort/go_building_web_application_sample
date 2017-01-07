1d8bbb0e478286746c293ac6e30b38342f84f8da
luccafort <luccafort@gmail.com>
2017-01-07 21:58:43 +0900

    [modify]テンプレート機能の実装
    flagパッケージの追加
    ServeHTTPのtemplate.Executeの第二引数にhttp.Responseを指定
    バイナリ起動時にコマンドライン引数でアドレスを指定できるよう変更

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -20,10 +21,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
@@ -31,7 +35,8 @@ func main() {
 	// チャットルームの開始
 	go r.run()
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Webサーバを開始します。ポート:", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServer", err)
 	}
 }
