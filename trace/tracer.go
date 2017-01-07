301a14a8796b3c49ba4fb4df693d0787aeb30e27
luccafort <luccafort@gmail.com>
2017-01-08 00:49:29 +0900

    [modify]Tracerに無効化機能を実装
    traceパッケージ利用者がtraceのOn/Offを任意で行えるよう修正
    Traceメソッドの呼び出しを無視するための関数と構造体を定義

--- trace/tracer.go
+++ trace/tracer.go
@@ -14,6 +14,8 @@ type tracer struct {
 	out io.Writer
 }
 
+type nilTracer struct{}
+
 func (t *tracer) Trace(a ...interface{}) {
 	t.out.Write([]byte(fmt.Sprint(a...)))
 	t.out.Write([]byte("\n"))
@@ -21,4 +23,11 @@ func (t *tracer) Trace(a ...interface{}) {
 
 func New(w io.Writer) Tracer {
 	return &tracer{out: w}
+}
+
+func (t *nilTracer) Trace(a ...interface{}) {}
+
+// OffはTraceメソッドの呼び出しを無視するTracerを返します
+func Off() Tracer {
+	return &nilTracer{}
 }
