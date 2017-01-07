bcc6d7ce4b084a058ad3968b0e2485dadb6473bb
luccafort <luccafort@gmail.com>
2017-01-08 00:59:32 +0900

    [add]チャプター2の準備
    チャプター1のコードなどをコピー

---
+++ chapter_2/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+)
+
+// templは1つのテンプレートを表します
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServerHTTPはHTTPリクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+
+	r := newRoom()
+	//r.tracer = trace.New(os.Stdout)
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// チャットルームの開始
+	go r.run()
+
+	log.Println("Webサーバを開始します。ポート:", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServer", err)
+	}
+}
---
+++ chapter_2/room.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/luccafort/building_web_application/trace"
+)
+
+type room struct {
+	forward chan []byte      // forwardは他のクライアントに転送するためにメッセージを保持するチャネル
+	join    chan *client     // join はチャットルームに参加しようとしているクライアントのためのチャネル
+	leave   chan *client     // leave はチャットルームから退室しようとしているクライアントのためのチャネル
+	clients map[*client]bool // clients には在室している全てのクライアントが保持されます
+	tracer  trace.Tracer     // tracerはチャット上で行われた操作のログを受け取ります
+}
+
+// newRoom はすぐに利用できるチャットルームを生成して返す
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+		tracer:  trace.Off(),
+	}
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+			r.tracer.Trace("新しいクライアントが参加しました")
+		case client := <-r.leave:
+			// 退室
+			delete(r.clients, client)
+			close(client.send)
+			r.tracer.Trace("クライアントが退室しました")
+		case msg := <-r.forward:
+			r.tracer.Trace("メッセージを送信しました: ", string(msg))
+			// 全てのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージ送信
+					r.tracer.Trace(" -- クライアントに送信されました")
+				default:
+					delete(r.clients, client)
+					close(client.send)
+					r.tracer.Trace(" -- 送信に失敗しました。クライアントをクリーンアップします")
+				}
+			}
+		}
+	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() {
+		r.leave <- client
+	}()
+	go client.write()
+	client.read()
+}
