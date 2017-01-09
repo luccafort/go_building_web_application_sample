1afeb1a988f39fbe23651be7aa7891bc628a31cc
luccafort <luccafort@gmail.com>
2017-01-09 11:34:35 +0900

    [add]チャプター3を追加
    チャプター3ようのディレクトリを新規作成(chapter2のコピー)
    アバター画像のURLを取得する処理を実装

---
+++ chapter_3/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
+)
+
+// templは1つのテンプレートを表します
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServerHTTPはHTTPリクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCockie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCockie.Value)
+	}
+	t.templ.Execute(w, data)
+}
+
+func main() {
+	var addr = flag.String("host", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+
+	// Gomniauthのセットアップ
+	gomniauth.SetSecurityKey("セキュリティーキー")
+	gomniauth.WithProviders(
+		facebook.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/facebook"),
+		github.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/github"),
+		google.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/google"),
+	)
+
+	r := newRoom()
+	//r.tracer = trace.New(os.Stdout)
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.Handle("/room", r)
+
+	// assets設定
+	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets/"))))
+
+	// チャットルームの開始
+	go r.run()
+
+	log.Println("Webサーバを開始します。ポート:", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServer", err)
+	}
+}
---
+++ chapter_3/message.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"time"
+)
+
+// message は1つのメッセージを表します
+type message struct {
+	Name      string
+	Message   string
+	When      time.Time
+	AvatarURL string
+}
