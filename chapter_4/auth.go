bee5f513b758d192c96a310cd45ae1ead903eb9b
luccafort <luccafort@gmail.com>
2017-01-10 01:36:57 +0900

    [add]チャプター4追加
    チャプター3のコピペ

---
+++ chapter_4/auth.go
@@ -0,0 +1,115 @@
+package main
+
+// 具象型をインターフェースに置換
+import gomniauthcommon "github.com/stretchr/gomniauth/common"
+import (
+	"crypto/md5"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"io"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/objx"
+)
+
+type ChatUser interface {
+	UniqueID() string
+	AvatarURL() string
+}
+
+type chatUser struct {
+	gomniauthcommon.User
+	uniqueID string
+}
+
+func (u chatUser) UniqueID() string {
+	return u.uniqueID
+}
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// クッキーがない、もしくは値が空のときはログインへ
+	if cookie, err := r.Cookie("auth"); err == http.ErrNoCookie || cookie.Value == "" {
+		// 未認証
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else if err != nil {
+		// 何らかの別のエラーが発生
+		panic(err.Error())
+	} else {
+		// 成功。ラップされたハンドラを呼び出します
+		h.next.ServeHTTP(w, r)
+	}
+}
+
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
+
+// LoginHandlerはサードパーティーへのログインの処理を受け持ちます
+// パスの形式: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln("認証プロバイダの取得に失敗しました:", provider, "-", err)
+		}
+		loginURL, err := provider.GetBeginAuthURL(nil, nil)
+		if err != nil {
+			log.Fatalln("GetBeginAuthURLの呼び出し中にエラーが発生しました:", provider, "-", err)
+		}
+		w.Header().Set("Location", loginURL)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	case "callback":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln("認証プロバイダーの取得に失敗しました", provider, "-", err)
+		}
+
+		creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		if err != nil {
+			log.Fatalln("認証を完了できませんでした", provider, "-", err)
+		}
+
+		user, err := provider.GetUser(creds)
+		if err != nil {
+			log.Fatalln("ユーザーの取得に失敗しました", provider, "- ", err)
+		}
+
+		chatUser := &chatUser{User: user}
+		m := md5.New()
+		io.WriteString(m, strings.ToLower(user.Email()))
+		chatUser.uniqueID = fmt.Sprintf("%x", m.Sum(nil))
+		avatarURL, err := avatars.GetAvatarURL(chatUser)
+		if err != nil {
+			log.Fatalln("GetAvatarURL()に失敗しました", "-", err)
+		}
+
+		authCockieValue := objx.New(map[string]interface{}{
+			"userid":     chatUser.uniqueID,
+			"name":       user.Name(),
+			"avatar_url": avatarURL,
+			"email":      user.Email(),
+		}).MustBase64()
+		http.SetCookie(w, &http.Cookie{
+			Name:  "auth",
+			Value: authCockieValue,
+			Path:  "/",
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "アクション'%s'には非対応です", action)
+	}
+}
---
+++ chapter_4/client.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+// clientはチャットを行っている1人のユーザを表します
+type client struct {
+	socket   *websocket.Conn        // socketはこのクライアントのためのWebSocketです
+	send     chan *message          // sendはメッセージが送られます
+	room     *room                  // roomがこのクライアントが参加しているチャットルームです
+	userData map[string]interface{} // userData はユーザに関する情報を保持します
+}
+
+func (c *client) read() {
+	for {
+		var msg *message
+		if err := c.socket.ReadJSON(&msg); err == nil {
+			msg.When = time.Now()
+			msg.Name = c.userData["name"].(string)
+			if avatarURL, ok := c.userData["avatar_url"]; ok {
+				msg.AvatarURL = avatarURL.(string)
+			}
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
+
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteJSON(msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
