215cc4eaff6d97d062177366741db8c569f5c913
luccafort <luccafort@gmail.com>
2017-01-18 17:27:52 +0900

    [fix]代入式を修正
    buildエラーになっていたため代入処理を修正

--- chapter_5/socialpoll/twittervotes/main.go
+++ chapter_5/socialpoll/twittervotes/main.go
@@ -61,7 +61,7 @@ var db *mgo.Session
 func dialdb() error {
 	var err error
 	log.Println("MongoDBにダイアル中: localhost")
-	db, err := mgo.Dial("localhost")
+	db, err = mgo.Dial("localhost")
 	return err
 }
 
