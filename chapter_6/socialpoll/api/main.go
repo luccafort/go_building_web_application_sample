5ab96d80607ec1cbc66ae83019b165b492536227
luccafort <luccafort@gmail.com>
2017-01-19 11:33:37 +0900

    [modify]DB接続実装
    SetVar関数を用いてDBセッションを管理を実装

--- chapter_6/socialpoll/api/main.go
+++ chapter_6/socialpoll/api/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"net/http"
+
+	"gopkg.in/mgo.v2"
 )
 
 func main() {}
 
-func withAPIKey(fn http.HandlerFunc) http.HandleFunc {
+func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if !isValidAPIKey(r.URL.Query().Get("key")) {
 			respondErr(w, r, http.StatusUnauthorized, "不正なAPIキーです")
@@ -18,4 +20,12 @@ func withAPIKey(fn http.HandlerFunc) http.HandleFunc {
 
 func isValidAPIKey(key string) bool {
 	return key == "abc123"
+}
+func withData(d *mgo.Session, fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		thisDb := d.Copy()
+		defer thisDb.Close()
+		SetVar(r, "db", thisDb.DB("ballots"))
+		fn(w, r)
+	}
 }
