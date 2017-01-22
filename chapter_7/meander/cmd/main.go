6a5c7c7dc45398b5ced391a404cfa2f96739ddb4
luccafort <luccafort@gmail.com>
2017-01-22 12:47:02 +0900

    [fix]スペルミスを修正
    HandleFuncの第二引数の型指定をミスっていたので修正

--- chapter_7/meander/cmd/main.go
+++ chapter_7/meander/cmd/main.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"net/http"
 	"runtime"
+
+	"github.com/luccafort/building_web_application/chapter_7/meander"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	// meander.APIKey = "TODO"
-	http.HandleFunc("/journyes", func(w http.ResponseWriter, r http.Request) {
+	http.HandleFunc("/journyes", func(w http.ResponseWriter, r *http.Request) {
 		respond(w, r, meander.Journeys)
 	})
 	http.ListenAndServe(":8080", http.DefaultServeMux)
