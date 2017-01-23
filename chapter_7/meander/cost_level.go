467fd0b06ee81cb8f79890e372c654b8b3c074cb
luccafort <luccafort@gmail.com>
2017-01-23 10:47:30 +0900

    [add]列挙型の定義
    meanderの列挙型定数とその文字列変換関数などを追加
    列挙型定数などの変換処理のテストコードを追加

---
+++ chapter_7/meander/cost_level.go
@@ -0,0 +1,56 @@
+package meander
+
+import (
+	"strings"
+)
+
+type Cost int8
+
+const (
+	_ Cost = iota
+	Cost1
+	Cost2
+	Cost3
+	Cost4
+	Cost5
+)
+
+const CostSeparate string = "..."
+
+var costStrings = map[string]Cost{
+	"$":     Cost1,
+	"$$":    Cost2,
+	"$$$":   Cost3,
+	"$$$$":  Cost4,
+	"$$$$$": Cost5,
+}
+
+func (l Cost) String() string {
+	for s, v := range costStrings {
+		if l == v {
+			return s
+		}
+	}
+	return "不正な値です"
+}
+
+func ParseCost(s string) Cost {
+	return costStrings[s]
+}
+
+type CostRange struct {
+	From Cost
+	To   Cost
+}
+
+func (r CostRange) String() string {
+	return r.From.String() + CostSeparate + r.To.String()
+}
+
+func ParseCostRange(s string) *CostRange {
+	segs := strings.Split(s, CostSeparate)
+	return &CostRange{
+		From: ParseCost(segs[0]),
+		To:   ParseCost(segs[1]),
+	}
+}
