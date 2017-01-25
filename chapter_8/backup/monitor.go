d4d7568c0bf8cd36396c86903a2fe6527a9ed5b6
luccafort <luccafort@gmail.com>
2017-01-25 12:20:14 +0900

    :sparkles: [add]モニタリング機能の実装
    モニタリング対象を扱う構造体を定義
    変更を検知する関数を追加
    バックアップを開始する関数を追加

---
+++ chapter_8/backup/monitor.go
@@ -0,0 +1,39 @@
+package backup
+
+import "path/filepath"
+import "fmt"
+import "time"
+
+// Monitor モニタリング対象の変更検知とバックアップを実行管理
+type Monitor struct {
+	Paths       map[string]string // ファイルパス(絶対パス)
+	Archiver    Archiver          // アーカイバ
+	Destination string            // アーカイバの保存先
+}
+
+// Now 対象をチェック、変更がある個数を返す
+func (m *Monitor) Now() (int, error) {
+	var counter int
+	for path, lastHash := range m.Paths {
+		newHash, err := DirHash(path)
+		if err != nil {
+			return 0, err
+		}
+		if newHash != lastHash {
+			err := m.act(path)
+			if err != nil {
+				return counter, err
+			}
+			m.Paths[path] = newHash
+			counter++
+		}
+	}
+	return counter, nil
+}
+
+// act バックアップを開始
+func (m *Monitor) act(path string) error {
+	dirname := filepath.Base(path)
+	filename := fmt.Sprintf("%d.zip", time.Now().UnixNano())
+	return m.Archiver.Archive(path, filepath.Join(m.Destination, dirname, filename))
+}
